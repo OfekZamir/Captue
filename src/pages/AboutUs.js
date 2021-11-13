716b73b1ab5eb0670dadab18d1eacfe75e0c14c4
OfekZamir <zamirofek132@gmail.com>
2021-11-13 20:08:13 +0200

    AboutSection

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,8 @@
+import react from "react";
+//Page Componentes
+import { AboutSection } from "../components/AboutSection";
+
+const AboutUS = () => {
+  return <AboutSection />;
+};
+export default AboutUS;

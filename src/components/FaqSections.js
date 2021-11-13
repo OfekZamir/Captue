9d339006323c3c8b3ae15dc02ae353c2396c6add
OfekZamir <zamirofek132@gmail.com>
2021-11-13 20:25:43 +0200

    FaqSection

---
+++ src/components/FaqSections.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+export const FaqSections = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any questions? <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I Start?</h4>
+        <div className="answer">
+          <p>test test</p>
+          <p>aljdfsahdlsjakdsad</p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>test test</p>
+          <p>aljdfsahdlsjakdsad</p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Diferrent Payment Methodes</h4>
+        <div className="answer">
+          <p>test test</p>
+          <p>aljdfsahdlsjakdsad</p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What Products Do You Offer?</h4>
+        <div className="answer">
+          <p>test test</p>
+          <p>aljdfsahdlsjakdsad</p>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,14 @@
 //Page Componentes
 import { AboutSection } from "../components/AboutSection";
 import { ServicesSection } from "../components/ServicesSection";
+import { FaqSections } from "../components/FaqSections";
 
 const AboutUS = () => {
   return (
     <>
       <AboutSection />
       <ServicesSection />
+      <FaqSections />
     </>
   );
 };

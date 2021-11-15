7a4e3a82fa5a818d1c2ca2aa738da09ec70b70b5
OfekZamir <zamirofek132@gmail.com>
2021-11-15 10:54:40 +0200

    Creating styled components to aboutSection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+//GlobalStyle
+import GlobalStyle from "./components/GlobalStyle";
 //import Pages
 import AboutUS from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUS />
     </div>
   );

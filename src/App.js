81982461db71c45f0895eefb45b9829968109e86
OfekZamir <zamirofek132@gmail.com>
2021-11-25 21:23:55 +0200

    adding react router and links, on click going to moviedetail and shit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,32 @@ import React from "react";
 import GlobalStyle from "./components/GlobalStyle";
 //import Pages
 import AboutUS from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+import { Nav } from "./components/Nav";
+import MovieDetail from "./pages/MovieDetail";
+//Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <AboutUS />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUS />
+        </Route>
+        <Route path="/work" exact>
+          <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
